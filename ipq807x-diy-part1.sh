c913489c659eefe774863a07b2a867abe9b7196f
lihao25 <lihao25@meituan.com>
2023-03-01 15:29:00 +0800

    update

--- ipq807x-diy-part1.sh
+++ ipq807x-diy-part1.sh
@@ -6,7 +6,7 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: lede_diy-part1.sh
+# File name: ipq807x-diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
